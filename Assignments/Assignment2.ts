bd9f2b80fe166a8dfc9968726bc7ec039b3b9411
Sru Swapna Ganpisetti <sruswapnaganpisetti295@gmail.com>
2026-02-13 15:29:25 +0530

    Assignment2

---
+++ Assignments/Assignment2.ts
@@ -0,0 +1,36 @@
+let customerName: string= '"Swapna"';
+let creditScore:number=720;
+let income:number=55000;
+let isEmployed: boolean= true;
+let debtToIncomeRatio: number= 35.0;
+
+console.log("The Customer Name is: "+customerName);
+if( creditScore>750){
+    console.log(" You are eligile for loan. Loan will Approved");
+} 
+else if(creditScore>650 && creditScore<750){
+   console.log("There are some Additional checks to be performed");
+   if(income>50000){
+    console.log("We will Consider The Application");
+    if ( isEmployed==true){
+        console.log("Still You Have Chance to get Loan");
+        if( debtToIncomeRatio<40){
+            console.log("You Will Get The Loan");
+        }
+        if(debtToIncomeRatio>=40){
+            console.log("Loan will Denied because of DIT");
+        }
+    }else{
+        console.log("Loan will Denied because of Unemployement");
+    }
+
+   }
+   else{
+         console.log("Loan will Denied because of less income")
+   }
+     
+}
+
+else{
+ console.log("Loan will Denied");
+}

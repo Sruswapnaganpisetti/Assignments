f11005f6e93ca7111628d4fd1d43c2a2e6dfaa0c
Sru Swapna Ganpisetti <sruswapnaganpisetti295@gmail.com>
2026-02-20 18:15:39 +0530

    Assignemt9

--- Assignments/Assignment9.ts
+++ Assignments/Assignment9.ts
@@ -1,14 +1,16 @@
-let rows: number = 5;
-
+function printPattern(rows:number):void{
 for (let i: number = 1; i <= rows; i++) {
 
-    let pattern: string = "";
+    let pattern: string = "";  
     for (let j: number = 1; j <= rows - i; j++) {
         pattern += " ";
     }
-    for (let k: number = 1; k <= i; k++) {
+    for (let k: number = 0; k < i; k++) {
         pattern += "*";
     }
 
     console.log(pattern);
 }
+}
+printPattern(5);
+printPattern(6);
